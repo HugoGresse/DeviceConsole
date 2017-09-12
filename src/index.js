bd6f6c31a31e2e703738460f35a455bce7d0a240
Hugo Gresse <hugo.gresse@teads.tv>
2017-09-12 14:25:04 +0200

    Add GA

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux"
 import { Router, Route, IndexRedirect, browserHistory } from "react-router";
+import ReactGA from 'react-ga';
 
 import { initAuth } from "./core/auth"
 import store from "./core/store"
@@ -17,13 +18,13 @@ import './index.css';
 function render() {
     ReactDOM.render(
         <Provider store={store}>
-            <Router history={browserHistory}>
+            <Router history={browserHistory} onUpdate={logPageView}>
 
                 <Route path="/login" component={Login} onEnter={requireUnauth(store.getState)} />
 
                 <Route path="/" component={App} onEnter={requireAuth(store.getState)} >
                     <IndexRedirect to="devices" />
-                    <Route path="devices" name="Devices" component={Devices}/>
+                    <Route path="devices" name="Devices" component={Devices} />
                 </Route>
 
             </Router>
@@ -33,6 +34,17 @@ function render() {
 }
 
 // registerServiceWorker();
+
+
+function logPageView() {
+    if (process.env.NODE_ENV === 'development') {
+        return;
+    }
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+}
+
+ReactGA.initialize('UA-106330269-1');
 
 initAuth(store.dispatch)
     .then(() => render())
